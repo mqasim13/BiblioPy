6f9cb2e0504df621cb49e281e32dcda8c79f1bbc
MuhammadQasim Dewji <mqasim13@gmail.com>
2015-10-22 12:09:00 -0700

    Started working toward barebones program

---
+++ BiblioPy.py
@@ -0,0 +1,19 @@
+## BIBLIOPY ##
+# A COMMAND LINE BASED CITATION GENERATOR #
+
+#Asks user for the type of citation needed 
+Cittype: raw_input("Please indicate method of citation (i.e. APA, Chicago, MLA): ")
+Type = raw_input("Please indicate type of citation (i.e. book, website, pdf, journal, newspaper, magazine, film): ")
+
+Authfirst = raw_input("Please enter author's first name: ")
+Authlast = raw_input("Please enter author's last name: ")
+Title = raw_input("Please enter the title: ")
+print "If there is no volume # enter 0"
+Volume = raw_input("Please enter the volume #: ")
+print "If there is no edition # enter 0"
+Edition = raw_input("Please enter the edition #: ")
+PageNum = raw_input("Please enter page numbers: ")
+
+
+## MLA CITATION ## 
+
